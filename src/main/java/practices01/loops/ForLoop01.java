5f64499b493c705ed1249ba11c0e44251f733b79
bilalaymak <bilalaymak@gmail.com>
2022-12-18 09:31:14 +0300

    updated

---
+++ src/main/java/practices01/loops/ForLoop01.java
@@ -0,0 +1,101 @@
+package practices01.loops;
+
+public class ForLoop01 {
+    public static void main(String[] args) {
+
+
+        //Example 1: type code to reverse a String
+        // for example: Top ==> moT  ==> very common interview question
+
+        String name = "Mark";
+        String reversedName = ""; // we created new container to save new data
+
+        for (int i = name.length()-1 ; i >=0 ; i--) {
+            // to reverse a String we should use name.length()-1 method
+            char ch = name.charAt(i);// we create a container(ch) to simplify the code
+           // System.out.print(ch);//kraM ==> 1. way to print the result
+            reversedName = reversedName + ch;
+        }
+        System.out.println(reversedName);// we changed System.out.print(ch);//kraM
+        // 2. way to print the result
+        // and write a sout out of the for loop to take and print the reversedName output
+
+        System.out.println("*********************");
+
+
+        String name2 = "Bilal";
+        String reversedName2 = "";
+        for (int k = name2.length()-1; k >=0 ; k--) {
+
+            char ch2 = name2.charAt(k);
+            reversedName2 = reversedName2 + ch2;
+
+        }
+        System.out.println(reversedName2);
+
+        //Example 2: find the sum of the integers from 3 to 6
+
+        int sum = 0;
+
+        for (int m = 3; m < 7 ; m++) {
+            sum = sum + m;
+        }
+        System.out.println(sum);
+
+        //example 3: find the multiplication of the integers from 3 to 6
+        // interview question
+
+        int multiplication = 1;
+        for (int i = 3; i < 7 ; i++) {
+
+            multiplication = multiplication * i;
+        }
+        System.out.println(multiplication);
+
+
+        //example 4: type code to find the sum of the digits of an integer
+        // 574 ==> 5+7+4=16
+
+        int num = 574;
+        num = Math.abs(num);
+        int result = 0;
+
+        for (int i = 0; i < 3; i++) {
+            result = result + num%10;
+
+            num = num/10;
+        }
+        System.out.println(result);
+
+        //second way
+
+         num = -584; // we used negative number
+        num = Math.abs(num);
+        result = 0;
+        for (int i = 584; i >0 ; i=i/10) {
+            result = result + i%10;
+        }
+        System.out.println(result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/practices01/loops/ForLoop02.java
@@ -0,0 +1,93 @@
+package practices01.loops;
+
+import java.util.Scanner;
+
+public class ForLoop02 {
+    public static void main(String[] args) {
+
+        //example 1: type code to print characters except "m" in a String
+        // for example, Andromeda ==> Androeda
+
+        String str = "Andromeda";
+
+        //1. way
+        for (int i = 0; i < str.length(); i++) {
+            // when we use how many character are there
+            //in a String, we use str.length(); method
+            char ch = str.charAt(i);
+            //we use charAt(): method to take pointed character of the String
+            if(ch!= 'm'){
+                System.out.print(ch);//Androeda
+            }
+        }
+
+        System.out.println("********************");
+        //2. way
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch=='m'){
+                continue;// if you want to skip any value in lopp, use "continue"
+            }
+            System.out.print(ch);
+            }
+
+        System.out.println();
+        System.out.println("***************");
+        //example 2: type code to print characters before "m" in a String
+
+        str = "Luxembourg";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == 'm'){
+                break;// if you want to break any loop under some conditions
+                // you can use "break" keyword
+            }
+            System.out.print(ch);//Luxe
+        }
+
+        System.out.println();
+        //example 3: type code to find the sum of the unique digits in an integer
+        //  for example, 1232 ==> 1+3 =4 we don't use the second 2, because it is used before
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter an integer to see the sum of the unique digits in it");
+         String num = input.next();
+
+        int sum = 0;
+        for (int i = 0; i < num.length() ; i++) {
+            String digit = num.substring(i,i+1);//to get wanted character(i,i+1)
+            if(num.indexOf(digit) == num.lastIndexOf(digit)){
+
+           //num.indexOf(digit) == num.lastIndexOf(digit) is the rule to be unique
+                sum =  sum + Integer.valueOf(digit);// we used Integer.valueOf();
+                // method to convert
+                // the String to the int value
+            }
+
+        }
+        System.out.println(sum);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
