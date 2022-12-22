7bbb75187692cc6d461d950f138ddbfa7a0155a2
bilalaymak <bilalaymak@gmail.com>
2022-12-22 07:02:26 +0300

    updated

---
+++ src/main/java/practices01/loops/whileloops/DoWhile03.java
@@ -0,0 +1,25 @@
+package practices01.loops.whileloops;
+
+public class DoWhile03 {
+    public static void main(String[] args) {
+        /*
+        type code to print digits just in the decimal part of the given decimal number with an asterix
+        for example; 75.4238 ==> *4*2*3*8
+         */
+
+        double num = 75.4238;
+        String str = String.valueOf(num);
+        String s = ""; //start point like int sum = 0; replace of String
+        int i = 0;
+        String decPart = str.split("\\.")[1];//when we use just "." in split() method
+        // we must use backward slash (\\.)
+
+        System.out.println("decimal part is " + decPart);
+
+        do {
+          String r = decPart.substring(i,i+1);
+         s = s + "*" + r;
+         i++;
+        }while (i < decPart.length());
+    }
+}
---
+++ src/main/java/practices01/loops/whileloops/DoWhileLoop02.java
@@ -0,0 +1,53 @@
+package practices01.loops.whileloops;
+
+import java.util.Scanner;
+
+public class DoWhileLoop02 {
+    public static void main(String[] args) {
+        /*
+        username is "admin", password is "pwd123"
+        ask user to enter username and password
+        user should see "Enter your username and password" message
+        if he enters correct credentials he should get "you are in your account!"
+        otherwise, he should see "enter your username and password" message 3 times
+        after 3 times he should get "your account is blocked" message
+         */
+
+
+         Scanner input = new Scanner(System.in);
+        int counter = 0; //to enter to program
+
+        do {
+            if (counter ==4){
+                System.out.println("your account is blocked!");
+                break;
+            }
+            System.out.println("enter your username");
+            String username = input.next();
+
+            System.out.println("enter your password");
+            String password = input.next();
+
+            if (username.equals("admin") && password.equals("pwd123")){
+                System.out.println("you are in your account!");
+                break;
+            }
+            counter++;
+        }while (true); //infinite loop
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
