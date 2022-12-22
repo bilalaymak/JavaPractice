e5d3755e495bca63aaad64427ebecd6afa7f38f8
bilalaymak <bilalaymak@gmail.com>
2022-12-22 09:53:42 +0300

    updated

---
+++ src/main/java/practices01/constructersmethodcallsstatickeywords/constructers/Static01.java
@@ -0,0 +1,58 @@
+package practices01.constructersmethodcallsstatickeywords.constructers;
+
+public class Static01 {
+
+
+      /*
+      1) "static" class members(variables and methods) are common for all objects created from the class
+      2) When you do any update on "static" class members, all objects will see the updates on it
+      3) if you want every object informed about every update on a class member make it static
+       */
+
+        //how to create static variable
+        //to make a variable "static", put "static" keyword between "access modifier" and "data type"
+
+        public static int staticCounter = 0;
+        public int nonStaticCounter = 0;
+
+        public Static01(){
+         staticCounter++;
+         nonStaticCounter++;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
