e1cfd6afc9417959b2c6f11ab29ddb6f2f7ca6c5
bilalaymak <bilalaymak@gmail.com>
2022-12-25 19:52:21 +0300

    updated

--- src/main/java/practices01/arrays/arraysforeachloop/Arrays02.java
+++ src/main/java/practices01/arrays/arraysforeachloop/Arrays02.java
@@ -1,6 +1,7 @@
 package practices01.arrays.arraysforeachloop;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Arrays02 {
     public static void main(String[] args) {
@@ -19,6 +20,52 @@ public class Arrays02 {
 
         System.out.println("Month: " + dateArray[0] + " Day: " + dateArray[1] + " Year: " + dateArray[2]);
         //Month: 09 Day: 20 Year: 2022
+
+        System.out.println("***************************");
+
+        //example 2: get string from user and type code to find the number of words in the String
+        // "I like to move it, move it" ==> 7
+
+        // we find the number of words in a String by using split method
+
+        String str = "I like to move it, move it";
+        String arr[] = str.split(" ");
+        System.out.println("the number of the words is " + arr.length );//the number of the words is 7
+
+        System.out.println("********************************");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("type a sentence");
+        String s = input.nextLine();
+        String brr[] = s.split(" ");
+        System.out.println("the number of the words is " + brr.length);//the number of the words is 7
+
+
+        System.out.println("*************************");
+
+        //Example 3: count the number of words starts with "a" in the String
+        //"I am from argentina" there are two words start with "a" // brr = [I,am,from,argentina]
+        int counter = 0;// variables to check are called "flag"
+
+        for(String w: brr){
+            if(w.startsWith("a") || w.startsWith("A")){// we used this kind of using to make ignoreCase
+                counter++;
+            }
+        }
+        System.out.println(counter + " words start with a");//2 words start with a
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
