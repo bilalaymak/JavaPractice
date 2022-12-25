12b22774a30cb9a6120e6823d5964bc69fc50c55
bilalaymak <bilalaymak@gmail.com>
2022-12-25 22:53:47 +0300

    updated

---
+++ src/main/java/practices01/arrays/multidimensionalarrays/MultiDimensionalArrays03.java
@@ -0,0 +1,37 @@
+package practices01.arrays.multidimensionalarrays;
+
+public class MultiDimensionalArrays03 {
+    public static void main(String[] args) {
+
+        //convert multidimensional array to ane dimensional array
+        //{ {"learn","java","it"},{"is","easy"} } ==> {"learn","java","it","is","easy"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
