a94d65208ca8e72ccbec74f29aabd957cda57835
bilalaymak <bilalaymak@gmail.com>
2022-12-25 23:44:18 +0300

    updated

---
+++ src/main/java/practices01/arrays/multidimensionalarrays/MultiDimensionalArrays04.java
@@ -0,0 +1,27 @@
+package practices01.arrays.multidimensionalarrays;
+
+public class MultiDimensionalArrays04 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
