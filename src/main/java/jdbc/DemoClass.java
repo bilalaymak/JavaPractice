b0bf3a370b594dc694da4a603e4a00bf85222939
bilalaymak <bilalaymak@gmail.com>
2023-03-05 02:52:34 +0300

    updated

--- src/main/java/jdbc/DemoClass.java
+++ src/main/java/jdbc/DemoClass.java
@@ -1,6 +1,6 @@
 package jdbc;
 
-import java.sql.*;
+import java.sql.*; //1. step import packages
 
 public class DemoClass {
     public static void main(String[] args) {
@@ -11,36 +11,35 @@ public class DemoClass {
         String query = "SELECT * FROM students";
 
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName("org.postgresql.Driver"); //2. step load and register the driver
+            //3. step create and establishing the connection
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("baglanti basarili");//baglanti basarili
-            Statement st = connection.createStatement();
-            ResultSet rs = st.executeQuery(query);
+
+
+            Statement st = connection.createStatement(); //4. step creating the statement
+            ResultSet rs = st.executeQuery(query);//5. step executing the query
 
             while (rs.next()) {
-                String userData = rs.getString(1) + " : " + rs.getString(2);
-                System.out.println(userData);
+                String userData = rs.getString(1) + " : " +
+                        rs.getString(2) + " : " +rs.getString(3) +
+                        " : " +rs.getString(4) + " : " + rs.getString(5 );
+                System.out.println(userData); //6. step process the result
             }
             st.close();
-            connection.close();
+            connection.close();//7. step close the connection
 
             /*
-            baglanti basarili
-120 : Ali Can
-121 : Veli Han
-122 : Ayşe Tan
-123 : Mary Star
-124 : Terry Star
-125 : Herry Star
-126 : Sally Otar
+ baglanti basarili
+120 : Ali Can : Ankara : 70 : 2023-02-11
+121 : Veli Han : Trabzon : 75 : 2022-02-11
+122 : Ayşe Tan : Bursa : 85 : 2021-08-11
+123 : Mary Star : İstanbul : 95 : 2020-01-01
+124 : Terry Star : Ankara : 95 : 2019-09-01
+125 : Herry Star : Antalya : 95 : 2018-02-01
+126 : Sally Otar : İzmir : 100 : 2017-06-30
+
              */
-
-
-
-
-
-
-
 
         } catch (Exception e) {
             e.printStackTrace();
