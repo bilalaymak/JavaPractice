f022742ddf669385976375ce437dad7c94407d6d
bilalaymak <bilalaymak@gmail.com>
2023-03-05 23:38:58 +0300

    updated

---
+++ src/main/java/practices01/arraylists/arraylistwithclasses/Customer.java
@@ -0,0 +1,16 @@
+package practices01.arraylists.arraylistwithclasses;
+
+public class Customer {
+
+    int id;
+    String name;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
